cc622aa8dd8cbb12e07cc5cfecee272dd6ec35f4
Skial Bainn <skial.bainn@gmail.com>
2014-10-07 10:42:41 +0100

    Convert to abstract enum.

--- src/uhx/http/impl/e/EMethod.hx
+++ src/uhx/http/impl/e/EMethod.hx
@@ -5,12 +5,12 @@ package uhx.http.impl.e;
  * @author Skial Bainn
  */
 
-enum EMethod {
-	GET;
-	HEAD;
-	POST;
-	PUT;
-	DELETE;
-	OPTIONS;
-	PATCH;
+@:enum abstract EMethod(String) from String to String {
+	public var GET = 'get';
+	public var HEAD = 'head';
+	public var POST = 'post';
+	public var PUT = 'put';
+	public var DELETE = 'delete';
+	public var OPTIONS = 'options';
+	public var PATCH = 'patch';
 }
